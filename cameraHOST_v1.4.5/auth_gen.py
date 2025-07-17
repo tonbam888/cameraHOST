ed483ec53eba5be5a212d933ba05756b516ed3ff
tonbam888 <tonbam888@gmail.com>
2025-07-18 01:18:43 +0300

    Delete cameraHOST_v1.4.5 directory

--- cameraHOST_v1.4.5/auth_gen.py
+++
@@ -1,5 +0,0 @@
-import bcrypt
-
-password = input("Напиши пароль который будет использоваться:")
-hashed = bcrypt.hashpw(password.encode(), bcrypt.gensalt())
-print("Это пароль в шифровоном виде:", '\n', hashed.decode())
